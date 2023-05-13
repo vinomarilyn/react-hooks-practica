af5548015bfd3de40a20a8731d5491cbcf3bc53a
marilyn <vinomarilyn@gmail.com>
2023-05-13 08:52:12 -0300

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
-import React,{useState} from 'react';
+import React,{useState, useEffect} from 'react';
 function App() {
   const [counter, setCounte ] =useState(0); //regresa dos valores
+  useEffect(() => {
+    setCounte(count + 1);
+    return () => {
+      console.log('Cleanup')//resetea todas las variables
+    }
+  },[])//componentDidMount
+
   return (
     <div className="App">
       <h1>{counter}</h1>
